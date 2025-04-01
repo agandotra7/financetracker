d5c05ab19e8337e5bc8da930f175e1597798ae64
Anoushka Gandotra <anoushka107@gmail.com>
2025-04-01 13:33:51 -0400

    added base template, header, footer, and front-end

--- financetracker/settings.py
+++ financetracker/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'financetracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'moviesstore/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'financetracker/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'financetracker/static/',
+]
